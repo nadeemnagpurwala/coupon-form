5d40c4a49203f8e9a71156cc9ee78f2e13c13a38
Nadeem Nagpurwala <nagpurwalanadeem@gmail.com>
2020-10-10 17:31:05 +0530

    Creating layout and contact form

---
+++ script.js
@@ -0,0 +1,5 @@
+function addCardClass() {
+	let element = document.getElementById("card-animation");
+  	element.classList.remove("coupon-content");
+  	element.classList.add("coupon-content-up");
+}
